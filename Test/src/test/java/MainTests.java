b755e12f442811dd6f63d43a2181330437596f7a
Maite de la Roca <delaroca.estrada21@gmail.com>
2019-02-28 18:27:28 -0600

    tests files added

--- Test/src/test/java/MainTests.java
+++ Test/src/test/java/MainTests.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+//import nombre
+//import dpi
+//import edad
+//import genero
+//import telefono
+//import origen
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class MainTests {
+
+    @Test
+    public void nombre(){
+        //Nombre nombre = new Nombre
+        //String value=Main.nombre();
+        //Assert.assertNotNull(value);
+    }
+
+    @Test
+    public void carne(){
+        //Carne carne = new Carne();
+        //String value=Main.
+    }
+
+    @Test
+    public void edad(){
+        //Edad edad = new Edad();
+        //String value=Main.
+    }
+
+    @Test
+    public void genero(){
+        //Genero genero = new Genero();
+        //String value=Main.
+    }
+
+    @Test
+    public void telefono(){
+        //Telefono telefono = new Telefono;
+        //String value=Main.
+    }
+
+    @Test
+    public void origen(){
+        //Origen origen = new Origen();
+        //String value=Main.
+    }
+
 }
